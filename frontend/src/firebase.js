3f6e69d7df656c2368b1c2d9a4f3316d3bfa10e7
Tarun <tarun@example.com>
2026-05-02 20:36:49 +0530

    chore: hardcode firebase config for easier deployment

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBw-DUOtB5WG4tI9Fgax3jDnd8rVTp9vrQ",
+  authDomain: "team-task-manager-8cfb8.firebaseapp.com",
+  projectId: "team-task-manager-8cfb8",
+  storageBucket: "team-task-manager-8cfb8.firebasestorage.app",
+  messagingSenderId: "161148662773",
+  appId: "1:161148662773:web:8f0f316ea1a99126f766bc"
 };
 
 const app = initializeApp(firebaseConfig);
